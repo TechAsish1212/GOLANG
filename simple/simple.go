b004513b7922647f21c71ac598bf4fcc441a186b
TechAsish1212 <gitasish1212@gmail.com>
2026-05-30 23:26:59 +0530

    feat: learn how simple something

---
+++ simple/simple.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// int
+	fmt.Println(1+1);
+
+	// string
+	fmt.Println("This is a simple Go program.");
+
+	// boolean
+	fmt.Println(true);
+
+	// float
+	fmt.Println(3.14*0.3);
+
+	
+}
